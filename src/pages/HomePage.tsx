b57433738ed529b9df98d1bc6732696fa6d8b22e
alinandrei74 <72814555+alinandrei74@users.noreply.github.com>
2025-06-30 00:34:27 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -48,7 +48,7 @@ function HomePage() {
         setIsAuthModalOpen={setIsAuthModalOpen}
       />
       
-      <div className="absolute top-40 right-4 z-50">
+      <div className="absolute top-90 right-4 z-50">
         <a 
           href="https://bolt.new/" 
           target="_blank" 
